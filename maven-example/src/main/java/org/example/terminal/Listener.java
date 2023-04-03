421b4d8092757819d0d4504f8c511b61c50b06cc
Ugo Chukwu <ugochukwu@stripe.com>
2023-04-03 10:20:07 -0400

    Implement a TeminalListener for the stripe terminal sdk that logs events to the console

---
+++ maven-example/src/main/java/org/example/terminal/Listener.java
@@ -0,0 +1,32 @@
+package org.example.terminal;
+
+import com.stripe.stripeterminal.external.callable.TerminalListener;
+import com.stripe.stripeterminal.external.models.ConnectionStatus;
+import com.stripe.stripeterminal.external.models.PaymentStatus;
+import com.stripe.stripeterminal.external.models.Reader;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A simple {@link TerminalListener} implementation that just logs events to the console.
+ */
+public class Listener implements TerminalListener {
+  private ConnectionStatus connectionStatus = null;
+  private PaymentStatus paymentStatus = null;
+
+  @Override
+  public void onConnectionStatusChange(@NotNull ConnectionStatus status) {
+    TerminalListener.super.onConnectionStatusChange(status);
+    System.out.printf("onConnectionStatusChange from %1$s -> %2$s\n", connectionStatus, status);
+  }
+
+  @Override
+  public void onPaymentStatusChange(@NotNull PaymentStatus status) {
+    TerminalListener.super.onPaymentStatusChange(status);
+    System.out.printf("onPaymentStatusChange from %1$s -> %2$s\n", paymentStatus, status);
+  }
+
+  @Override
+  public void onUnexpectedReaderDisconnect(@NotNull Reader reader) {
+    System.out.printf("onUnexpectedReaderDisconnect from %1$s", reader);
+  }
+}
