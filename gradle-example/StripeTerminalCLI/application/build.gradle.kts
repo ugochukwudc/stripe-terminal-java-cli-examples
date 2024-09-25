8244513be16a4294939e6eff73f2a72d8714ecb0
Ugo Chukwu <ugochukwu@stripe.com>
2024-09-25 06:53:58 -0700

    upgrade to b8

--- gradle-example/StripeTerminalCLI/application/build.gradle.kts
+++ gradle-example/StripeTerminalCLI/application/build.gradle.kts
@@ -8,13 +8,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-//    maven(url = "https://d37ugbyn3rpeym.cloudfront.net/terminal/java-betas")
+    maven(url = "https://d37ugbyn3rpeym.cloudfront.net/terminal/java-betas")
 }
 
 dependencies {
-    implementation("com.stripe:stripeterminal-java:1.0.0-b1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.stripe:stripeterminal-java:1.0.0-b8")
 }
 
 tasks.test {
